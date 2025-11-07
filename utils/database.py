bbc9286f1f22608fdf90de42094df84c2d6e87e1
Umunna Chibuenyim <beingchris21@gmail.com>
2025-11-07 15:17:44 +0100

    remove PostgreSQL engine and psycopg2-binary

--- utils/database.py
+++ utils/database.py
@@ -5,10 +5,7 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL.startswith("sqlite"):
-    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread":False})
-else:
-    engine = create_engine(DATABASE_URL)    
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread":False})  
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
